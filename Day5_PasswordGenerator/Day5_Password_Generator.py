89f54541bf94f39cc4e71d4133eece1aea41b203
justmamba <mamba1994@yahoo.com>
2025-10-13 02:01:54 -0500

    Add Day 5 Password Generator

---
+++ Day5_PasswordGenerator/Day5_Password_Generator.py
@@ -0,0 +1,25 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+##goes through the lists and assigns a symbol/ letter or number too password
+password = ""
+for i in range(nr_letters):
+    password += random.choice(letters)
+for i in range(nr_symbols):
+    password += random.choice(symbols)
+for i in range(nr_numbers):
+    password += random.choice(numbers)
+print(password)
+
+##this was step 2, or the hard version, basically to randomize the passsword
+password2 = list(password)
+random.shuffle(password2)
+random_password ="".join(password2)
+print(random_password)
